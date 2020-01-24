98dd95d13436aab33b0d1a43dd0c73a309c87e49
ailishb <37984176+ailishb@users.noreply.github.com>
2020-01-24 18:25:07 -0300

    Demo of fetching test message from backend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/example')
+        .then(response => response.text())
+        .then(message => {
+            setMessage(message);
+        });
+  },[])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            {message}
+        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
