fce343390dad029e6da3c3d57b8afacd0320b5ff
ailishb <37984176+ailishb@users.noreply.github.com>
2020-02-02 17:50:40 -0300

    Added ContactFind and ContactDetail views, with navigation

--- src/components/ContactFind.js
+++ src/components/ContactFind.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import SearchIcon from "@material-ui/icons/Search";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import Chip from '@material-ui/core/Chip';
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import Chip from "@material-ui/core/Chip";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -25,15 +25,15 @@ const useStyles = makeStyles(theme => ({
   formControl: {
     margin: theme.spacing(1),
     minWidth: 120,
-    maxWidth: 300,
+    maxWidth: 300
   },
   chips: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap"
   },
   chip: {
-    margin: 2,
-  },
+    margin: 2
+  }
 }));
 
 const ITEM_HEIGHT = 48;
@@ -42,52 +42,25 @@ const MenuProps = {
   PaperProps: {
     style: {
       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
+      width: 250
+    }
+  }
 };
 
-const names = [
-  'OSU',
-  'GHC',
-  'Capstone',
-  'CS361',
-  'Meetup',
-];
+const tags = ["OSU", "GHC", "Capstone", "CS361", "Meetup"];
 
-function getStyles(name, personName, theme) {
-  return {
-    fontWeight:
-      personName.indexOf(name) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium,
-  };
-}
-
-export default function ContactFind() {
+export default function ContactFind(props) {
   const classes = useStyles();
-  const theme = useTheme();
-  const [personName, setPersonName] = React.useState([]);
+  const [selectedTag, setSelectedTag] = React.useState([]);
 
   const handleChange = event => {
-    setPersonName(event.target.value);
-  };
-
-  const handleChangeMultiple = event => {
-    const { options } = event.target;
-    const value = [];
-    for (let i = 0, l = options.length; i < l; i += 1) {
-      if (options[i].selected) {
-        value.push(options[i].value);
-      }
-    }
-    setPersonName(value);
+    setSelectedTag(event.target.value);
   };
 
   return (
     <div>
-      <Grid container spacing={1} align-content={"center"}>
-        <Grid item xs={4}>
+      <Grid container spacing={1}>
+        <Grid item lg={4} m={4} xs={12} style={{ textAlign: "center" }}>
           <form className={classes.root} noValidate autoComplete="off">
             <TextField
               id="search-contacts"
@@ -107,14 +80,14 @@ export default function ContactFind() {
           </form>
         </Grid>
 
-        <Grid item xs={4}>
+        <Grid item lg={4} m={4} xs={12} style={{ textAlign: "center" }}>
           <FormControl className={classes.formControl}>
             <InputLabel id="demo-mutiple-chip-label">Filter By Tags</InputLabel>
             <Select
               labelId="demo-mutiple-chip-label"
               id="demo-mutiple-chip"
               multiple
-              value={personName}
+              value={selectedTag}
               onChange={handleChange}
               input={<Input id="select-multiple-chip" />}
               renderValue={selected => (
@@ -126,27 +99,29 @@ export default function ContactFind() {
               )}
               MenuProps={MenuProps}
             >
-              {names.map(name => (
-                <MenuItem key={name} value={name} style={getStyles(name, personName, theme)}>
-                  {name}
+              {tags.map(tag => (
+                <MenuItem key={tag} value={tag}>
+                  {tag}
                 </MenuItem>
               ))}
             </Select>
           </FormControl>
         </Grid>
 
-        <Grid item xs={4}>
+        <Grid item lg={4} m={4} xs={12} style={{ textAlign: "center" }}>
           <Button
             variant="contained"
             color="primary"
             size="large"
             className={classes.button}
             startIcon={<AddCircleIcon />}
+            onClick={() => {
+              props.setContactView("ContactDetail");
+            }}
           >
             Add New Contact
           </Button>
         </Grid>
-
       </Grid>
     </div>
   );
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -7,6 +7,7 @@ import ContactTable from "./ContactTable";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import ContactDetail from "./ContactDetail";
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -18,9 +19,6 @@ const useStyles = makeStyles(theme => ({
     display: "flex",
     overflow: "auto",
     flexDirection: "column"
-  },
-  fixedHeight: {
-    height: 240
   }
 }));
 
@@ -28,14 +26,26 @@ export default function Contacts() {
   const classes = useStyles();
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
+  // Code to control which page is shown to user
+  let contactViewComponent = null;
+  const [contactView, setContactView] = React.useState("ContactFind");
+  switch (contactView) {
+    case "ContactFind":
+      contactViewComponent = <ContactFind setContactView={setContactView} />;
+      break;
+    case "ContactDetail":
+      contactViewComponent = <ContactDetail setContactView={setContactView} />;
+      break;
+    default:
+      contactViewComponent = <h1>No page selected...</h1>;
+  }
+
   return (
     <Container maxWidth="lg" className={classes.container}>
       <Grid container spacing={3}>
         {/* Action Area */}
         <Grid item xs={12}>
-          <Paper className={fixedHeightPaper}>
-            <ContactFind />
-          </Paper>
+          <Paper className={fixedHeightPaper}>{contactViewComponent}</Paper>
         </Grid>
         {/* Contacts */}
         <Grid item xs={12}>
