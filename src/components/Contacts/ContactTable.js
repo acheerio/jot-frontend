66b0facc753d409e6d317eff67257dccf8d4c022
acheerio <acheerioo@gmail.com>
2020-02-10 14:13:29 -0600

    working pagination from page 0, some icons

--- src/components/Contacts/ContactTable.js
+++ src/components/Contacts/ContactTable.js
@@ -1,5 +1,42 @@
 import React from "react";
+import { forwardRef } from 'react';
 import MaterialTable from 'material-table';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+const tableIcons = {
+    /*
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />), */
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    /* ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />) */
+};
 
 export default function ContactTable() {
     return (
@@ -13,24 +50,33 @@ export default function ContactTable() {
                     {title: 'Tags', field: null},
                     {title: 'Recent Updates', field: null},
                 ]}
+                icons={tableIcons}
+                options={{
+                    pageSize: 5,
+                    initialPage: 0,
+                    defaultSort: 'desc',
+                }}
                 data={query =>
                     new Promise((resolve, reject) => {
+                        query.orderBy = 'contactId';
+                        query.orderDirection = 'desc';
                         // let url = 'http://localhost:8080/contacts/all?userId=1&pageNum=0&pageSize=20&sortField=contactId&sortDirection=DESC';
                         let url = 'http://localhost:8080/contacts/all?'
                         // let url = 'http://api.jot-app.com/contacts?'
                         url += 'userId=1';
-                        url += '&sortField=contactId';
-                        url += '&sortDirection=DESC';
+                        url += '&sortField=' + query.orderBy;
+                        url += '&sortDirection=' + query.orderDirection;
                         url += '&pageSize=' + query.pageSize;
-                        url += '&pageNum=' + (query.page + 1);
+                        url += '&pageNum=' + query.page;
                         console.log(query);
                         fetch(url)
                             .then(response => response.json())
                             .then(result => {
-                                console.log(result.content)
+                                // console.log(result.content)
+                                // console.log(result)
                                 resolve({
                                     data: result.content,
-                                    page: result.page - 1,
+                                    page: result.number,
                                     totalCount: result.totalElements,
                                 })
                             })
