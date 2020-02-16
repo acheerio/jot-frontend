370059f10e600d56cdb7c26394fa5004b63c8afe
acheerio <acheerioo@gmail.com>
2020-02-15 21:16:24 -0600

    styled login button and page

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,48 +1,57 @@
 import React from "react";
-// import useScript from '../../util/useScript.js';
+import useScript from '../../util/useScript.js';
 import GoogleLogin from 'react-google-login';
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles(theme => ({
     container: {
         paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(4)
+        paddingBottom: theme.spacing(4),
+        alignItems: "center",
+        justify: "center",
+        textAlign: "center"
     },
     paper: {
-        padding: theme.spacing(2),
-        display: "flex",
+        padding: theme.spacing(4),
         overflow: "auto",
-        flexDirection: "column"
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    },
+    box: {
+        display: "flex",
+        alignItems: "center"
+    },
+    typography: {
+        textAlign: "center",
     }
 }));
 
 export default function Login() {
     const classes = useStyles();
-    // useScript('https://apis.google.com/js/platform.js', false, true);
+    useScript('https://apis.google.com/js/platform.js', false, true);
     let clientId = '924098505527-ta2u3pvgjksj497p9lu7rcahkvfoq1vs.apps.googleusercontent.com';
     const responseGoogle = (response) => {
         console.log(response);
     };
 
     return (
-
-        <Container maxWidth="lg" className={classes.container}>
+        <Container maxWidth="lg">
+            <meta name="google-signin-client_id" content={clientId}></meta>
             <Grid container spacing={3}>
-                {/* Action Area */}
                 <Grid item xs={12}>
                     <Paper className={classes.paper}>
-                        <GoogleLogin
-                            clientId={clientId}
-                            buttonText="Register or Login with Google"
-                            onSuccess={responseGoogle}
-                            onFailure={responseGoogle}
-                            cookiePolicy={'single_host_origin'}
-                        />
+                        <Typography variant="h2" component="h2" className={classes.typography}>Sign up for Jot today!</Typography>
                     </Paper>
                 </Grid>
+                <Grid item xs={5}></Grid>
+                <Grid item xs={2}>
+                    <Box component="div" className="g-signin2" data-onsuccess="onSignIn"></Box>
+                </Grid>
+                <Grid item xs={5}></Grid>
             </Grid>
         </Container>
     );
