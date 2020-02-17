151dd112c097607f28489459e97e8415464afc7f
acheerio <acheerioo@gmail.com>
2020-02-17 12:00:25 -0600

    manual login styled but no auth token

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -12,21 +12,15 @@ const useStyles = makeStyles(theme => ({
     container: {
         paddingTop: theme.spacing(4),
         paddingBottom: theme.spacing(4),
-        alignItems: "center",
-        justify: "center",
-        textAlign: "center"
+        display: "flex"
     },
     paper: {
         padding: theme.spacing(4),
         overflow: "auto",
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    },
-    box: {
-        display: "flex",
-        alignItems: "center"
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
     },
     typography: {
-        textAlign: "center",
+        textAlign: "center"
     }
 }));
 
@@ -37,11 +31,26 @@ export default function Login() {
     const responseGoogle = (response) => {
         console.log(response);
     };
+    function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+    }
 
     return (
         <Container maxWidth="lg">
+            <meta name="google-signin-scope" content="profile email https://www.googleapis.com/auth/contacts"></meta>
             <meta name="google-signin-client_id" content={clientId}></meta>
-            <Grid container spacing={3}>
+            <Grid className={classes.container} container spacing={3}>
                 <Grid item xs={12}>
                     <Paper className={classes.paper}>
                         <Typography variant="h2" component="h2" className={classes.typography}>Sign up for Jot today!</Typography>
@@ -49,7 +58,7 @@ export default function Login() {
                 </Grid>
                 <Grid item xs={5}></Grid>
                 <Grid item xs={2}>
-                    <Box component="div" className="g-signin2" data-onsuccess="onSignIn"></Box>
+                    <Box component="div" className="g-signin2" onClick={onSignIn}></Box>
                 </Grid>
                 <Grid item xs={5}></Grid>
             </Grid>
