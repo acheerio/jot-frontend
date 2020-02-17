38d9453b2695a2783ad485557dfa9e1588dacae4
acheerio <acheerioo@gmail.com>
2020-02-17 14:30:11 -0600

    working google login with getbasicprofile

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import useScript from '../../util/useScript.js';
 import GoogleLogin from 'react-google-login';
 import { makeStyles } from "@material-ui/core/styles";
@@ -7,6 +7,8 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
+
+/* global gapi */
 
 const useStyles = makeStyles(theme => ({
     container: {
@@ -25,31 +27,31 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function Login() {
-    const classes = useStyles();
-    useScript('https://apis.google.com/js/platform.js', false, true);
-    let clientId = '924098505527-ta2u3pvgjksj497p9lu7rcahkvfoq1vs.apps.googleusercontent.com';
-    const responseGoogle = (response) => {
-        console.log(response);
-    };
-    function onSignIn(googleUser) {
-        // Useful data for your client-side scripts:
-        var profile = googleUser.getBasicProfile();
-        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-        console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
-        console.log("Image URL: " + profile.getImageUrl());
-        console.log("Email: " + profile.getEmail());
 
-        // The ID token you need to pass to your backend:
-        var id_token = googleUser.getAuthResponse().id_token;
-        console.log("ID Token: " + id_token);
+    const classes = useStyles();
+
+    useEffect(() => {
+
+        gapi.signin2.render('g-signin2', {
+            'scope': 'profile email https://www.googleapis.com/auth/contacts',
+            'width': 200,
+            'height': 50,
+            'longtitle': false,
+            'theme': 'light',
+            'onsuccess': onSignIn
+        })
+    }, [])
+
+    function onSignIn(googleUser) {
+        let profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     }
 
     return (
         <Container maxWidth="lg">
-            <meta name="google-signin-scope" content="profile email https://www.googleapis.com/auth/contacts"></meta>
-            <meta name="google-signin-client_id" content={clientId}></meta>
             <Grid className={classes.container} container spacing={3}>
                 <Grid item xs={12}>
                     <Paper className={classes.paper}>
@@ -58,7 +60,7 @@ export default function Login() {
                 </Grid>
                 <Grid item xs={5}></Grid>
                 <Grid item xs={2}>
-                    <Box component="div" className="g-signin2" onClick={onSignIn}></Box>
+                    <Box component="div" id="g-signin2" className="g-signin2"></Box>
                 </Grid>
                 <Grid item xs={5}></Grid>
             </Grid>
